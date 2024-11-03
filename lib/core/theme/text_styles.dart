bfd5c1c9540c4532854875a51785352f96718efc
benik <galstyan.benik@gmail.com>
2024-11-03 13:53:12 +0200

    added text styles

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:todo/generated/assets/fonts.gen.dart';
+
+class TextStyles {
+  static const TextStyle buttonTextStyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: FontFamily.sFUIDisplay,
+  );
+}
