73858775b3b6d59befe24aada921c66a53747ea9
benik <galstyan.benik@gmail.com>
2024-11-03 20:11:35 +0200

    implemented update task status method

--- lib/data/repository/repository.dart
+++ lib/data/repository/repository.dart
@@ -2,4 +2,6 @@ import 'package:todo/data/models/task_model.dart';
 
 abstract interface class Repository {
   Future<List<Task>> getTasks();
+
+  Future<void> updateTaskStatus(String taskId, int newStatus);
 }
