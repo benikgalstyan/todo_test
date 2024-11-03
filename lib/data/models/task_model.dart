b71a11d760684cd22ad4d4613037aba690c47abe
benik <galstyan.benik@gmail.com>
2024-11-03 13:53:02 +0200

    implemented task model

---
+++ lib/data/models/task_model.dart
@@ -0,0 +1,40 @@
+class Task {
+  Task({
+    required this.taskId,
+    required this.status,
+    required this.name,
+    required this.type,
+    required this.finishDate,
+    required this.urgent,
+    this.description,
+  });
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      taskId: json['taskId'] as String,
+      status: json['status'] as int,
+      name: json['name'] as String,
+      type: json['type'] as int,
+      finishDate: DateTime.parse(json['finishDate'] as String),
+      urgent: json['urgent'] as int,
+      description: json['description'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'taskId': taskId,
+        'status': status,
+        'name': name,
+        'type': type,
+        'finishDate': finishDate.toIso8601String(),
+        'urgent': urgent,
+        'description': description,
+      };
+  final String taskId;
+  final int status;
+  final String name;
+  final int type;
+  final DateTime finishDate;
+  final int urgent;
+  final String? description;
+}
