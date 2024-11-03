29cf47c7e93de94e7ae690477cefaa2c995508c6
benik <galstyan.benik@gmail.com>
2024-11-03 20:11:47 +0200

    implemented main bloc

---
+++ lib/presentation/pages/main_screen/bloc/main_state.dart
@@ -0,0 +1,22 @@
+part of 'main_bloc.dart';
+
+@immutable
+abstract class MainState {}
+
+class MainInitialState extends MainState {}
+
+class MainUpdatingState extends MainState {}
+
+class MainUpdatedState extends MainState {}
+
+class MainLoadedState extends MainState {
+  MainLoadedState({required this.tasks});
+
+  final List<Task> tasks;
+}
+
+class MainErrorState extends MainState {
+  MainErrorState(this.exception);
+
+  final Object exception;
+}
