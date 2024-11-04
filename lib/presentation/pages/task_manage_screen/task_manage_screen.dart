353de4ea50d2f97dcfa24e636234b1f79c1497f9
benik <galstyan.benik@gmail.com>
2024-11-04 19:32:57 +0200

    implemented edit task screen

--- lib/presentation/pages/task_manage_screen/task_manage_screen.dart
+++ lib/presentation/pages/task_manage_screen/task_manage_screen.dart
@@ -1,16 +1,18 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/data/models/task_model.dart';
 import 'package:todo/data/repository/repository.dart';
 import 'package:todo/presentation/pages/main_screen/bloc/main_bloc.dart';
-import 'package:todo/presentation/pages/task_creation_screen/task_creation_layout.dart';
+import 'package:todo/presentation/pages/task_manage_screen/task_manage_layout.dart';
 import 'package:todo/service_locator.dart';
 
 @RoutePage()
-class TaskCreationScreen extends StatelessWidget {
-  const TaskCreationScreen({super.key, required this.taskId});
+class TaskManageScreen extends StatelessWidget {
+  const TaskManageScreen({super.key, this.task, required this.tasks});
 
-  final int taskId;
+  final Task? task;
+  final List<Task> tasks;
 
   static const nameRoute = '/CreateScreen';
 
@@ -18,7 +20,10 @@ class TaskCreationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => MainBloc(getIt<Repository>()),
-      child: TaskCreationLayout(taskId: taskId),
+      child: TaskManageLayout(
+        task: task,
+        tasks: tasks,
+      ),
     );
   }
 }
